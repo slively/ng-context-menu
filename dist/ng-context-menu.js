fbeffd1f2b0c8b251caefb7c249ccacea25a51c1
Ian Walter <iankwalter@me.com>
2014-03-17 19:43:30 -0400

    Fixing issue with preventDefault on menuitem click

--- dist/ng-context-menu.js
+++ dist/ng-context-menu.js
@@ -33,10 +33,9 @@ angular
           });
         });
 
-        angular.element($window).bind('click', function(event) {
+        angular.element($window).bind('click', function() {
           if (opened) {
             $scope.$apply(function() {
-              event.preventDefault();
               close(menuElement);
             });
           }
